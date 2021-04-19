0317a5cbe7b3683ca94be6791934d494acfe7a82
habejo <abejohanna@gmail.com>
2021-04-19 21:43:54 +0800

    Learn DOM Interactions part 1

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,7 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
+      lastName: '',
+      //fullName: '',
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (this.name === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (this.name === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // },
+  },
+  computed: {
+    fullName() {
+      console.log('Running again..');
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    },
   },
   methods: {
     // incrementCtr() {
@@ -10,13 +47,34 @@ const app = Vue.createApp({
     // },
     // decrementCtr() {
     //   this.counter--;
-    // }     
+    // }
     incrementCtr(num) {
       this.counter = this.counter + num;
     },
     decrementCtr(num) {
       this.counter = this.counter - num;
-    }
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submitForm(event) {
+      // submitForm(event) {
+      // event.preventDefault();
+      alert('Submitted');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    resetInput() {
+      this.name = '';
+    },
+    outputFullName() {
+      console.log('Running again..');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Abejo';
+    },
   },
 });
 
